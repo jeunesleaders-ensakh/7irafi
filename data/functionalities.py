dabca938f842254379deac0b7ac8f2bfce5b1787
Achraf Mansari <contact@achrafmansari.com>
2025-07-06 21:16:18 +0100

    Modified the scraping loop and included another Shopify store

--- data/functionalities.py
+++ data/functionalities.py
@@ -35,6 +35,7 @@ def get_products_shopify_store_one_page(shopify_domain: str, results_limit: int 
     api_url = f"{shopify_domain}/products.json?limit={results_limit}&page={page_index}"
     response = requests.get(api_url)
     response.raise_for_status()
+    response.encoding = "utf-8"
 
     raw_products_list = response.json()['products']
 
@@ -43,12 +44,14 @@ def get_products_shopify_store_one_page(shopify_domain: str, results_limit: int 
     for product in raw_products_list:
         for variant in product['variants']:
             clean_data = {
+                "product_page": f"https://www.chabi-chic.com/products/{product['handle']}",
                 "name": f"{product['title']} - {variant['title']}",
                 "category": product['product_type'],
                 "price": variant['price'],
                 "weight": variant['grams'],
                 "tags": product['tags'],
-                "description": html_to_text(product['body_html']),
+                "description": html_to_text(product['body_html']) if product['body_html'] else "",
+                "images": [img["src"] for img in product['images']],
             }
 
             products_list.append(clean_data)
@@ -60,14 +63,47 @@ def get_products_shopify_store_all_pages(shopify_domain: str, results_limit: int
     curent_page = 1
     products_list = []
 
-    fetched_products = get_products_shopify_store_one_page(shopify_domain, results_limit, curent_page)
+    while True:
+        print(f"Scraping page {curent_page} from {shopify_domain}")
 
-    while len(fetched_products) > 0:
-        # Add the last fetched products to the products list
+        fetched_products = get_products_shopify_store_one_page(shopify_domain, results_limit, curent_page)
+
+        if len(fetched_products) == 0:
+            break
+
         products_list.extend(fetched_products)
 
         # Increment the current page counter to fetch data from the next page
         curent_page += 1
-        fetched_products = get_products_shopify_store_one_page(shopify_domain, results_limit, curent_page)
 
     return products_list
+
+
+def scrape_beldimade_com():
+    PRODUCTS_SITEMAP_URL = "https://www.beldimade.com/product-sitemap.xml"
+
+    # Phase 1: Fetch all products URLs from the product sitemap page
+    res = requests.get(PRODUCTS_SITEMAP_URL, headers={"User-Agent": "Mozilla/5.0 (compatible; BeldiScraper/1.0)"})
+    res.raise_for_status()
+    soup = BeautifulSoup(res.content, "xml")
+    product_pages = [loc.text for loc in soup.find_all("loc") if '/en/produit' in loc.text]
+
+    # Phase 2: Visit and scrape data from each product page
+    for product_page in product_pages:
+        try:
+            resp = requests.get(product_page)
+            soup = BeautifulSoup(resp.content, "html.parser")
+
+            product = {}
+
+            # Title
+            title_tag = soup.find("h1", class_="post-title")
+            product["name"] = title_tag.get_text(strip=True) if title_tag else None
+
+            # Price
+            price_tag = soup.find("p", class_="price")
+            product["price"] = price_tag.get_text(strip=True) if price_tag else None
+
+        except Exception as e:
+            print(f"Error scraping {product_page}: {e}")
+            return None
--- data/scraper.py
+++ data/scraper.py
@@ -5,17 +5,17 @@ from data.functionalities import get_products_shopify_store_all_pages
 
 # Configurations
 DATA_FOLDER = 'data'
+os.makedirs(DATA_FOLDER, exist_ok=True)  # Ensure the data folder is existing
+
+# 1. Scraping data from Shopify stores
 STORES = [
     {"csv_filename": "artisans_du_maroc.csv", "base_url": "https://artisans-dumaroc.com/"},
     {"csv_filename": "chabi_chic.csv", "base_url": "https://www.chabi-chic.com/"},
+    {"csv_filename": "mytindy.csv", "base_url": "https://mytindy.com/"}
 ]
 
-# Ensure the data folder is existing
-os.makedirs(DATA_FOLDER, exist_ok=True)
-
-# Scraping data from Shopify stores
 for store in STORES:
-    store_products = get_products_shopify_store_all_pages(store["base_url"])
+    store_products = get_products_shopify_store_all_pages(store["base_url"], 250)
 
     with open(os.path.join(DATA_FOLDER, store["csv_filename"]), 'w', newline='', encoding='utf-8') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=store_products[0].keys())
